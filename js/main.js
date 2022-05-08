09c3062b609e836c482376c88251810907ebccfa
Elias Santander <cazagb@gmail.com>
2022-05-08 17:44:19 -0300

    small tweaks

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+const lista = document.getElementById("myUL");
+const creaitem = document.createElement("li");
+function toDoc() {
+  for (var i = 0; i < 1101; i++) {
+    creaitem.innerHTML = `<a href="#">${db[i].nombre} <span class="badge bg-success">${db[i].etiquetado}</span></a `
+    lista.appendChild(creaitem)
+  }
+}
+
+// Declare variables
+var input, filter, ul, li, a, i, txtValue;
+input = document.getElementById('myInput');
+filter = input.value.toUpperCase();
+ul = document.getElementById("myUL");
+li = ul.getElementsByTagName('li');
+
 function filterClients() {
-    // Declare variables
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-  
     // Loop through all list items, and hide those who don't match the search query
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
@@ -16,4 +25,4 @@ function filterClients() {
         li[i].style.display = "none";
       }
     }
-  }
+}
